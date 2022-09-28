1aef2219b768440f7b6bce8fe926abbc72ec8af5
FrancoT67 <francotoledo1235@gmail.com>
2022-09-28 09:55:55 -0300

    comletar el de los billetes

---
+++ ejercicioBanco.js
@@ -0,0 +1,16 @@
+/**
+ * Armar el algortmo para un cajero de banco. Este posee billetes de: 50,20,10,5 y 1. COn esto,el cajero debe:
+ * -Aceptar el monto de entrada(INT) que representa el monto a retirar.
+ * -Calcular y mostrar cuantos billetes de cada monto entrega en total.
+ *  EJEMPLO:
+ *       Monto a retirar: 221
+ *       Cantidad de billetes de 50: 4
+ *       Cantidad de billetes de 20: 1
+ *       Cantidad de billetes de 10: 0
+ *       Cantidad de billetes de 5: 0
+ *       Cantidad de billetes de 1: 1
+ */
+
+let valor=321
+
+console.log(Math.floor(valor/50))
