6066a4405f14a8807b2c518f1fe8850ea9cbdbd0
FrancoT67 <francotoledo1235@gmail.com>
2022-09-28 09:40:53 -0300

    added gitIgnore + finish ejercicioPiramide

--- ejercicioPiramide.js
+++ ejercicioPiramide.js
@@ -1,3 +1,21 @@
+/**
+ * Armar un programa que imprima una piramide de Mario Bros. El programa debe:
+ *  -Tomar como entrada la altura de la piramide(INT entre 2 y 8 inclusive).
+ *  -Imprimir dos semi piramides de la altura especificada con '#' que esten separadas por dos espacios.
+ *   EJEMPLO:
+ *       # #
+        ## ##
+       ### ###
+      #### ####
+     ##### #####
+    ###### ######
+   ####### #######
+  ######## ########
+ ######### #########
+########## ##########
+
+*/
+
 function piramide(x){
     let z=x
     for(let i=0;i<=x;i++){
