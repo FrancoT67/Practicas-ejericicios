f1c84150b382393eb1ef4df9fd996cd1f66070d0
Franco Toledo <francotoledo1235@gmail.com>
2022-09-28 18:45:54 -0300

    ejercicios resueltos, me costo el 2

--- ejercicioBanco.js
+++ ejercicioBanco.js
@@ -1,5 +1,5 @@
 /**
- * Armar el algortmo para un cajero de banco. Este posee billetes de: 50,20,10,5 y 1. COn esto,el cajero debe:
+ * Armar el algortmo para un cajero de banco. Este posee billetes de: 50,20,10,5 y 1. Con esto,el cajero debe:
  * -Aceptar el monto de entrada(INT) que representa el monto a retirar.
  * -Calcular y mostrar cuantos billetes de cada monto entrega en total.
  *  EJEMPLO:
@@ -11,6 +11,29 @@
  *       Cantidad de billetes de 1: 1
  */
 
-let valor=321
+function comprobarDinero(a){
+    let billetes=[50,20,10,5,1]
+    let resto=0
+    let cant=[]
+    let resultado=[]
+    if((a === a.toString()) || (a === true)|| (a === false)){
+        console.log('ingrese un numero entero')
+    }else{
+        console.log(`Monto a retirar:${a}`)
+        for(i=0;i<billetes.length;i++){
+            resultado.push(Math.floor(a/billetes[i]))
+            resto=a%billetes[i]
+            if(resto>0){
+                a=resto
+            }
+            console.log(`Cantidad de billetes de ${billetes[i]}: ${resultado[i]}`)
+        }
+        
 
-console.log(Math.floor(valor/50))
+    }
+
+        
+}
+
+let valor=221
+console.log(comprobarDinero(valor))
